7b31c13c118684f91899633021f07d04e2b33831
MrnoRobot <95551463+MrnoRobot0@users.noreply.github.com>
2022-11-29 22:51:28 +0100

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,39 @@
+from netaddr import valid_ipv4
+import termcolor
+import requests
+import json
+import sys
+
+
+class Tracker():
+    def __init__(self):
+        self.ip = None
+
+    def get_info(self):
+        r = json.loads(requests.get("https://ipinfo.io/{}/json".format(self.ip)).text)
+        info = {
+            "city": r['city'], 
+            "region": r['region'], 
+            "country":r['country'],
+            "loc": r['loc'], 
+            "postal": r['postal']
+        }
+
+        for i in info:
+            print(termcolor.colored("[+] ", "blue", attrs=['bold']) + termcolor.colored("{}: {}".format(i.capitalize(), info[i])))
+
+    def main(self):
+        if sys.argv[1:]:
+            if valid_ipv4(sys.argv[1]):
+                self.ip = sys.argv[1]
+                self.get_info()
+            else:
+                print(termcolor.colored("[-] Match Not Found as Ip", "red", attrs=['bold']))
+        else:
+            print(termcolor.colored("\n[-] Any Ip provided.", "red", attrs=['bold']))
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    t = Tracker()
+    t.main()
